36d2540f591e791a5878205be004738ae60b9408
AlexTchek <tchek@mail.ru>
2019-06-27 16:19:22 +0300

    modal show-error animation

--- js/index.js
+++ js/index.js
@@ -19,11 +19,46 @@ window.addEventListener("keydown", function (evt) {
     if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
     }
+
+    if (writeUsPopup.classList.contains("modal-show")) {
+      writeUsPopup.classList.remove("modal-show");
+      writeUsPopup.classList.remove("modal-error");
+    }
   }
   if (evt.keyCode === 13) {
     evt.preventDefault();
     if (!mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.add("modal-show");
     }
+  }
+});
+
+var writeUsLink = document.querySelector(".write-us-button");
+
+var writeUsPopup = document.querySelector(".modal-write-us");
+var writeUsClose = writeUsPopup.querySelector(".modal-close");
+
+var writeUsForm = writeUsPopup.querySelector("form");
+var writeUsName = writeUsPopup.querySelector("[name=name]");
+var writeUsEmail = writeUsPopup.querySelector("[name=email]");
+var writeUsText = writeUsPopup.querySelector("[name=text]");
+
+writeUsLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeUsPopup.classList.add("modal-show");
+  writeUsName.focus();
+});
+
+writeUsClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeUsPopup.classList.remove("modal-show");
+  writeUsPopup.classList.remove("modal-error");
+});
+
+writeUsForm.addEventListener("submit", function (evt) {
+  if (!writeUsName.value || !writeUsEmail.value || !writeUsText.value) {
+    evt.preventDefault();
+    writeUsPopup.classList.add("modal-error");
+    console.log("Нужно заполнить все поля");
   }
 });
